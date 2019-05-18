d3593733e6656424e01f0b10e0bba544b9bf4108
luizomatias <luizomatias1@yahoo.com.br>
2019-05-17 22:16:00 -0300

    Updates

---
+++ precache-manifest.2c57364b46cbb97e05ae42de5cdfb751.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b7cbe6c58e6fb1820291831982fa9a",
+    "url": "/robotfriends-pwa/index.html"
+  },
+  {
+    "revision": "c5460976e9dc21432f76",
+    "url": "/robotfriends-pwa/static/css/2.2ec98f22.chunk.css"
+  },
+  {
+    "revision": "cc4df0ffa144f4db8585",
+    "url": "/robotfriends-pwa/static/css/main.4ef126c0.chunk.css"
+  },
+  {
+    "revision": "c5460976e9dc21432f76",
+    "url": "/robotfriends-pwa/static/js/2.254b316b.chunk.js"
+  },
+  {
+    "revision": "cc4df0ffa144f4db8585",
+    "url": "/robotfriends-pwa/static/js/main.a0f0ce0e.chunk.js"
+  },
+  {
+    "revision": "827132a910aaad77c019",
+    "url": "/robotfriends-pwa/static/js/runtime~main.6b1db6ed.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends-pwa/static/media/SEGA.12a3db8b.woff"
+  }
+]);
